fb0bfec8b32bbd9c051220393db583c4b9199d9f
BurcuMengu <burcumengu@gmail.com>
2024-07-11 15:41:02 +0300

    push

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "05053302987Bb.",
+    port: "5432",
+});
 
 const app = express();
 const port = 3000;
 
-let quiz = [
-    { country: "France", capital: "Paris"},
-    { country: "United Kingdom", capital: "London" },
-    { country: "United States of America", capital: "New York" },
-];
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+    if(err) {
+        console.log("Error executing query", err.stack);
+    } else {
+        quiz = res.rows;
+    }
+    db.end();
+});
 
 let totalCorrect = 0;
 
@@ -20,7 +36,7 @@ let currentQuestion = {};
 
 //Get home page
 app.get("/", async (req, res) => {
-    totalCorrect =0;
+    totalCorrect = 0;
     await nextQuestion();
     console.log(currentQuestion);
     res.render("index.ejs", { question: currentQuestion });
